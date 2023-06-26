4ac9491924e598148b3367aab92acf19312acb69
paulo-fs <jr.paulo87@gmail.com>
2023-06-26 10:45:26 -0300

    chore: define theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,1 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Hello world</h1>
-    </main>
-  )
-}
+export { default } from './Home'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{ts,tsx,mdx}',
+    './src/components/**/*.{ts,tsx,mdx}',
+    './src/app/**/*.{ts,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', 'sans-serif'],
     },
+    colors: {
+      blue: {
+        main: '#0077F4',
+        light: '#E6F3FF',
+        background: '#F0F8FF',
+      },
+      black: '#1C3C50',
+      gray: '#627397',
+      white: '#fff',
+    },
+    extend: {},
   },
   plugins: [],
 }
