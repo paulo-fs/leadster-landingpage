057e76c8f3a3ef05cddc767029ed3f9dd4022b86
paulo-fs <jr.paulo87@gmail.com>
2023-06-27 13:29:25 -0300

    feat: add play icon asset

--- src/components/CardVideo/CardVideo.tsx
+++ src/components/CardVideo/CardVideo.tsx
@@ -4,12 +4,12 @@ import playIcon from '@/assets/playIcon.svg'
 
 export function CardVideo() {
   return (
-    <div className='max-w-card rounded-3xl shadow-2xl group relative'>
-      <div className='w-full h-full rounded-t-3xl group-hover:bg-blue-main transition'>
+    <div className='max-w-card rounded-3xl shadow-2xl group flex flex-col'>
+      <div className='w-full h-full rounded-t-3xl group-hover:bg-blue-main transition relative'>
         <Image data-img='bg' src={videoThumbnail} className='rounded-t-3xl group-hover:opacity-70 transition' alt='' />
+        <Image data-img='icon' src={playIcon} alt='' className='absolute top-20 right-36 opacity-0 group-hover:opacity-100 transition' />
       </div>
 
-      <Image data-img='icon' src={playIcon} alt='' className='absolute top-20 right-36 opacity-0 group-hover:opacity-100 transition' />
 
       <h6 className='py-6 px-8 text-base font-bold text-black group-hover:text-blue-main transition'>
           Como aumentar sua Geração de Leads feat. Tranktor
