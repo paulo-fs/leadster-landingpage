c4835a9c3b0b0d744c0252455ee780690a7f6da6
paulo-fs <jr.paulo87@gmail.com>
2023-06-28 14:27:25 -0300

    chore: add types for video data

---
+++ src/dataTypes/videos.dto.ts
@@ -0,0 +1,16 @@
+export interface IVideoData {
+  id: string
+  title: string
+  url: string
+  topic: Topics
+  date: string
+}
+
+export enum Topics {
+  CHATBOT = 'chatBot',
+  LEADS = 'leads',
+  AGENCY = 'agency',
+  PAIDMEDIA = 'paidMedia',
+  DIGITALMARKETING = 'digitalMarketing',
+  ALL = 'all',
+}
