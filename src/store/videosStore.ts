eee3afcb3542a12596e4bd4ce5719afb16547022
paulo-fs <jr.paulo87@gmail.com>
2023-06-28 17:28:16 -0300

    fix: selected topic when it is ALL

--- src/store/videosStore.ts
+++ src/store/videosStore.ts
@@ -5,22 +5,34 @@ interface VideoStoreProps {
   orderBy: string
   selectedTopic: Topics
   filteredVideos: IVideoData[] | null
+  allVideos: IVideoData[] | null
 
-  selectTopic: (topic: Topics, videos: IVideoData[]) => void
+  setVideos: (videos: IVideoData[]) => void
+  setTopic: (topic: Topics) => void
 }
 
 const initialState = {
   orderBy: 'date',
   selectedTopic: Topics.ALL,
   filteredVideos: null,
+  allVideos: null
 }
 
 export const useVideoStore = create<VideoStoreProps>((set, get) => ({
   ...initialState,
 
-  selectTopic: (topic: Topics, videos: IVideoData[]) => {
+  setVideos: (videos: IVideoData[]) => {
+    set({ allVideos: videos })
+  },
+
+  setTopic: (topic: Topics) => {
+    const {allVideos} = get()
+    if (topic === Topics.ALL) {
+      set({ selectedTopic: topic })
+      return set({ filteredVideos: null })
+    }
     set({ selectedTopic: topic })
-    const result = videos.filter((video) => video.topic === topic)
+    const result = allVideos?.filter((video) => video.topic === topic)
     set({ filteredVideos: result })
   },
 }))
