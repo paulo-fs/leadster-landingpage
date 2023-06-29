e6dbfd6fa648890d77d3958844edf587a79e925b
paulo-fs <jr.paulo87@gmail.com>
2023-06-29 11:50:03 -0300

    feat: implement jackarta typeface

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -3,15 +3,22 @@ import { Hero } from './sections/Hero/Hero'
 import { VideosSection } from './sections/Videos/Videos'
 import { CTA } from './sections/CTA/CTA'
 
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+export const jackarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
 
 export default function Home() {
   return (
-    <>
+    <div className={jackarta.className}>
       <HomeHeader />
       <Hero />
       <VideosSection />
       <CTA />
       <HomeFooter />
-    </>
+    </div>
   )
 }
