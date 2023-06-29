ec8f48c4d29741131fa005470116b4c5204077c4
paulo-fs <jr.paulo87@gmail.com>
2023-06-29 13:24:00 -0300

    fix: adjusts on props

--- src/components/CardVideo/CardVideo.tsx
+++ src/components/CardVideo/CardVideo.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import videoThumbnail from '@/assets/thumbnail.png'
 import playIcon from '@/assets/playIcon.svg'
+import { HTMLAttributes } from 'react'
 
-interface CardVideoProps {
+interface CardVideoProps extends HTMLAttributes<HTMLDivElement> {
   title: string
 }
 
-export function CardVideo(props: CardVideoProps){
-  const { title } = props
+export function CardVideo(compProps: CardVideoProps){
+  const { title, ...props } = compProps
 
   return (
-    <div className='max-w-card rounded-3xl shadow-2xl group flex flex-col'>
-      <div className='w-full h-full rounded-t-3xl group-hover:bg-blue-main transition relative'>
+    <div {...props} className='max-w-card rounded-3xl shadow-2xl group flex flex-col'>
+      <div className='w-full  rounded-t-3xl group-hover:bg-blue-main transition relative'>
         <Image data-img='bg' src={videoThumbnail} className='rounded-t-3xl group-hover:opacity-70 transition' alt='' />
         <Image data-img='icon' src={playIcon} alt='' className='absolute top-20 right-36 opacity-0 group-hover:opacity-100 transition' />
       </div>
