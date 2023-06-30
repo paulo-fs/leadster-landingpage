df1f20ca5a2da7b401a952f5c83fd8707e56d213
paulo-fs <jr.paulo87@gmail.com>
2023-06-30 08:52:35 -0300

    fix: on close modal

--- src/components/VideoModal/VideoModal.tsx
+++ src/components/VideoModal/VideoModal.tsx
@@ -1,19 +1,20 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
 import { Dialog } from '@headlessui/react'
 import ReactPlayer from 'react-player/lazy'
-import { Player } from '@lottiefiles/react-lottie-player';
-import { ButtonDownload } from "../ButtonDownload/ButtonDownload";
+import { Player } from '@lottiefiles/react-lottie-player'
+import { X } from 'phosphor-react'
+import { ButtonDownload } from '../ButtonDownload/ButtonDownload'
 import loadingAnimation from '@/assets/Loading-2.json'
 
-import { Plus_Jakarta_Sans } from "next/font/google";
-import { IVideoInfos } from "@/store/videosStore";
+import { Plus_Jakarta_Sans } from 'next/font/google'
+import { IVideoInfos } from '@/store/videosStore'
 
 export const jackarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  fallback: ["Helvetica", "Arial", "sans-serif"],
-});
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
 
 interface VideoModalProps {
   handleModal: Dispatch<SetStateAction<boolean>>
@@ -27,17 +28,28 @@ export function VideoModal({ handleModal, isOpen, videoInfos }: VideoModalProps)
   useEffect(() => setMounted(true), [])
 
   const modalContent = (
-    <Dialog open={isOpen} onClose={handleModal} className={`relative z-50 ${jackarta.className}`}>
+    <Dialog open={isOpen} onClose={handleModal} className={`relative z-10 ${jackarta.className}`}>
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-      <div className="fixed inset-0 flex items-center justify-center p-4">
+
+      <div className="fixed inset-0 flex items-center justify-center p-4 z-50">
         <Dialog.Panel  className="mx-auto max-w-xl rounded-2xl bg-white">
+
+          <div className='absolute w-full max-w-xl group'>
+            <button  className='absolute top-4 right-5 rounded-full p-2 bg-white group-hover:bg-blue-buttonMedium transition'
+              onClick={() => handleModal(false)}
+            >
+              <X size={24}  />
+            </button>
+          </div>
+
           <Dialog.Title className='py-9 px-20 text-lg font-medium'>
             <span className="text-blue-main">{videoInfos.isWebnar && 'Webinar:'} </span>
             {videoInfos.title}
           </Dialog.Title>
 
-          <div className="w-full relative">
+
+          <div className="w-full relative z-50">
             <ReactPlayer
               url={videoInfos.url}
               width='100%'
@@ -76,13 +88,13 @@ export function VideoModal({ handleModal, isOpen, videoInfos }: VideoModalProps)
               </div>
             </div>
           </div>
-        </Dialog.Panel>
 
-        <Dialog.Panel className="fixed bottom-0 left-0 bg-white p-6 flex flex-col gap-3">
-          <ButtonDownload type='Spreadsheet.xls' />
-          <ButtonDownload type='Document.doc' />
-          <ButtonDownload type='Presentation.ppt' />
-          <ButtonDownload type='Folder.zip' />
+          <div className="fixed bottom-0 left-0 bg-white p-6 flex flex-col gap-3">
+            <ButtonDownload type='Spreadsheet.xls' />
+            <ButtonDownload type='Document.doc' />
+            <ButtonDownload type='Presentation.ppt' />
+            <ButtonDownload type='Folder.zip' />
+          </div>
         </Dialog.Panel>
       </div>
     </Dialog>
